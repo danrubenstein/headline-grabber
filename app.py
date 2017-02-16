e6ad0c816e4171e5e2f306a38f54346efe9a9283
Dan Rubenstein <drubenstein13@gmail.com>
2017-02-15 19:38:01 -0600

    UX?

--- app.py
+++ app.py
@@ -174,6 +174,7 @@ def get_google_search_result(search_term):
     if r.status_code != 200:
         log("Bad search query")
         log(r.status_code)
+        log(r.text)
         return None
 
     soup = BeautifulSoup(r.text, "html.parser")
