723efa7c08553333824c04d5e92aaba93944378a
Dan Rubenstein <drubenstein13@gmail.com>
2017-02-27 17:47:44 -0600

    LOG everything

--- app.py
+++ app.py
@@ -36,9 +36,13 @@ def webhook():
 
     # endpoint for processing incoming messaging events
 
+    log(request)
+    log(request)
     data = request.get_json()
-    # log(data)  # you may not want to log every incoming message in production, but it's good for testing
-    
+    log(data)  # you may not want to log every incoming message in production, but it's good for testing
+
+
+
     if data["object"] == "page":
 
         for entry in data["entry"]:
